16c54a33dff0d42bd68566a6ab8840acdf4f3e9b
DESKTOP-A8GKV9V\DV <dmytro.vyshnevskyi@evinent.com>
2023-05-01 01:36:21 +0300

    algorithm maybe done

---
+++ Hedgehogs/HedgehogsService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hedgehogs
+{
+    internal class HedgehogsService
+    {
+        public int[] GetRandomHedgehogsPopulation()
+        {
+            Random random = new();
+
+            return new int[] { random.Next(0, int.MaxValue), random.Next(0, int.MaxValue), random.Next(0, int.MaxValue) };
+        }
+
+        public int[] GetRandomHedgehogsPopulation(int maxnumberForOneColor)
+        {
+            Random random = new();
+
+            return new int[] { random.Next(1, maxnumberForOneColor), random.Next(1, maxnumberForOneColor), random.Next(1, maxnumberForOneColor) };
+        }
+
+        public int CalulateStepsToSameColor(int[] hedgehogs,int color)
+        {            
+            checkArguments(hedgehogs);
+
+            var targetColorNumber = hedgehogs[color];
+
+            var sourceHedgehogs = hedgehogs.Where((x, i) => i != color);
+
+            var difference =  Math.Abs(sourceHedgehogs.First() - sourceHedgehogs.Last());
+
+            if (difference == 0)
+                return sourceHedgehogs.First();
+
+            if (difference % 3 != 0)
+                return -1;
+
+            if (difference / 3 > targetColorNumber)
+                return -1;
+
+            return hedgehogs.Min() + difference;
+        }
+
+      
+
+        private void checkArguments(IEnumerable<int> hedgehogs)
+        {
+            if (hedgehogs.Count() > 3)
+                throw new ArgumentException("There are only 3 color for hedgehogs");
+            if (hedgehogs.Count() == 0 || hedgehogs.All(x => x == 0))
+                throw new ArgumentException("No hedgehogs");
+        }
+    }
+}
--- Hedgehogs/Program.cs
+++ Hedgehogs/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+while (true)
+{
+    try
+    {
+        var hedgehogsService = new Hedgehogs.HedgehogsService();
+
+        var hedgehogs = new int[3];       
+
+        Console.WriteLine("Enter the number of target color (red - 0, green - 1, blue - 2):");
+
+        var targetColor = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the number hedgehogs in order of their color (red, green, blue):");
+
+        hedgehogs[0] = Int32.Parse(Console.ReadLine());
+        hedgehogs[1] = Int32.Parse(Console.ReadLine());
+        hedgehogs[2] = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Number of steps to color of to target color: {hedgehogsService.CalulateStepsToSameColor(hedgehogs, targetColor)}");
+
+        Console.WriteLine("End!\n\n\n\n");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
